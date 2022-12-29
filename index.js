4dccdef93559850d0eeb4bf4cd630355ae5ad9df
chamarabandarawd <chamarabandarawd@gmail.com>
2022-12-29 22:00:37 +0530

    Connected to the server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 import helmet from "helmet";
 import morgan from "morgan";
 
@@ -10,6 +10,10 @@ import generalRoutes from "./routes/general.js";
 import clientRoutes from "./routes/clients.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
+// import modules
+
+
+
 
 // Configuration
 dotenv.config();
@@ -27,3 +31,18 @@ app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+
+// MONGOOSE SETUP
+
+const PORT = process.env.PORT || 9000;
+mongoose.set('strictQuery', false); 
+mongoose 
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    })
+    .then(()=>{
+        app.listen(PORT,()=> console.log(`Server Port:${PORT}`));
+    })
+    .catch((error)=>console.log(`${error} did not connect` ));
